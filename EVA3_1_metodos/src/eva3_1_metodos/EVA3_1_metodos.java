9e096b14a0384519d6626c5beac5bf0c31e72710
CheemsDust <L24550259@chihuahua2.tecnm.mx>
2024-11-04 21:15:25 -0600

    Add files via upload

---
+++ EVA3_1_metodos/src/eva3_1_metodos/EVA3_1_metodos.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva3_1_metodos;
+
+/**
+ *
+ * @author calza
+ */
+public class EVA3_1_metodos {
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //uso de metodos
+        //invocacion o llamada
+        imprimirMensaje("Hola mundo!!");
+    }
+    
+    //header (encabezado)
+    //imprimir un mensaje
+    //JAVA:iniciar en minusculas, verbo, siguiente palabra inicial en mayusculas
+    public static void imprimirMensaje(String mensaje){
+        System.out.println("TU MENSAJE: " + mensaje);
+    }
+}
